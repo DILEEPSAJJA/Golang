b8665f215da6f5f85d91a87e95a18c2ed4782df0
DILEEPSAJJA <118289961+DILEEPSAJJA@users.noreply.github.com>
2023-04-26 08:29:35 +0530

    Add files via upload

---
+++ repo/datatype.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int;
+	var f float64;
+	var b bool;
+	var str string;
+
+	fmt.Printf("%T %T %T %T",i,f,b,str);
+	fmt.Printf("%v %v %v %q",i,f,b,str);
+
+}
