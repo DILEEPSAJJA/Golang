9be2ad065e086cd1046c66eb2f86b7c3b1d3c42b
DILEEPSAJJA <118289961+DILEEPSAJJA@users.noreply.github.com>
2023-05-18 16:08:18 +0530

    Add files via upload

---
+++ repo/myarray/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("My - Array Introduction")
+
+	var myFruitList [4]string
+
+	myFruitList[0] = "Apple"
+	myFruitList[1] = "Apple"
+	myFruitList[2] = "Apple"
+	myFruitList[3] = "Apple"
+
+	fmt.Println(myFruitList)
+
+}
