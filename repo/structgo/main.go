658bce768ab99a153d80165689bfd60d68be10ae
DILEEPSAJJA <118289961+DILEEPSAJJA@users.noreply.github.com>
2023-05-25 19:04:10 +0530

    Add files via upload

---
+++ repo/structgo/main.go
@@ -0,0 +1,18 @@
+package main
+
+import ("fmt")
+
+func main() {
+	fmt.Println("Struct Introduction")
+
+	dileep := User{"Dileep","dileep@gmail.com",true,20}
+	fmt.Println(dileep)
+
+}
+
+type User struct{
+	Name string
+	Email string
+	Status bool
+	Age int
+}
